1cfc6f22cb29953bf61220b5b34fc74471c82ad9
evenex <vlevenfeld@gmail.com>
2016-02-06 01:06:10 -0500

    First

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # simple_future
+
+Provides a (very) simple async function combinator.
+
+If `f :: A -> B`, then `async!f :: A -> Future!B`. 
+Suppose `f(a) = b` and `async!f(a) = c`.
+
+`async!f` will execute `f` in a separate thread when invoked.
+While `f` is being computed, `c.pending`.
+If `f` succeeds, `c.completed` and `b = c.result`.
+If `f` throws, `c.failed`.
+
+Attempting to read `c.result` before `c.completed` is an error.
---
+++ source/app.d
@@ -0,0 +1,145 @@
+module simple_future;
+  
+import core.thread;
+import std.concurrency;
+import std.traits;
+
+/**
+  Represents a value yet to be computed.
+  Its status can be queried (pending, failed, completed).
+  A Future fails If the function throws an exception or error.
+  A Future is completed once the async function that instantiated it has successfully returned a value.
+
+  The value computed by the async function can be retrieved from a completed Future with result().
+  The thread can wait for the Future to complete or fail by calling await().
+  await() returns the Future again, so it may be chained before a call to result();
+*/
+final class Future(T)
+if(!is(T == void))
+{
+public:
+
+  bool pending()() const
+  {
+    return state.status == Status.pending;
+  }
+  bool failed()() const
+  {
+    return state.status == Status.failed;
+  }
+  bool completed()() const
+  {
+    return state.status == Status.completed;
+  }
+
+  T result()()
+  in {
+    assert(this.completed);
+  } body {
+    return state.result;
+  }
+
+  Future await()()
+  {
+    while(this.pending)
+      Thread.yield;
+
+    return this;
+  }
+  
+  private{
+    this(){}
+
+    enum Status { pending, failed, completed }
+
+    struct State
+    {
+      T result;
+      Status status = Status.pending;
+    }
+    State state;
+  }
+}
+
+/**
+  Lifts a function to an asynchronous context.
+  Calling the lifted function will compute f in a new thread.
+
+  If the function, f, returned a T, then async!f will return a Future!T.
+*/
+template async(alias f)
+{
+  Future!(ReturnType!f) async(A...)(A args) // don't want to use ParamterTypeTuple in case f has default args
+  {
+    alias B = ReturnType!f;
+
+    auto future = new Future!B;
+
+    static auto run(
+      shared(Future!B) future
+      , A args
+    )
+    {
+      with((cast()future).state)
+      {
+        scope(success)
+          status = Future!B.Status.completed;
+        scope(failure)
+          status = Future!B.Status.failed;
+
+        result = f(args);
+      }
+    }
+
+    spawn(
+      &run, cast(shared)future, args
+    );
+
+    return future;
+  }
+}
+
+unittest
+{
+  static real square(real x)
+  {
+    return x*x;
+  }
+
+  auto fsq = async!square(5);
+
+  fsq.await;
+
+  assert(fsq.completed);
+  assert(fsq.result == 25);
+
+  static string wait100()
+  {
+    Thread.sleep(100.msecs);
+
+    return "done";
+  }
+
+  auto fwait = async!wait100;
+
+  assert(!fwait.completed);
+  assert(fwait.pending);
+
+  Thread.sleep(200.msecs);
+
+  assert(fwait.completed);
+  assert(fwait.result == "done");
+
+  static int crash()
+  {
+    throw new Exception("crashed");
+  }
+
+  auto fcrash = async!crash;
+
+  fcrash.await;
+
+  assert(!fcrash.completed);
+  assert(!fcrash.pending);
+  assert(fcrash.failed);
+}
